575c2d12ae1aacfdd4649ba11e0f736af0a93edd
Hans Evan Tatipata <thansevan@gmail.com>
2022-07-16 05:40:56 +0700

    Finishing

---
+++ src/utils/data.js
@@ -0,0 +1,30 @@
+const getData = () => {
+    return [
+      {
+        id: 0,
+        name: 'Hans Evan',
+        tag: 'hansevant',
+        imageUrl: '/images/connie.jpg',
+      },
+      {
+        id: 1,
+        name: 'Dimas Saputra',
+        tag: 'dimasmds',
+        imageUrl: '/images/luffy.jpg',
+      },
+      {
+        id: 2,
+        name: 'Arif Faizin',
+        tag: 'arifaizin',
+        imageUrl: '/images/morant.jpg',
+      },
+      {
+        id: 3,
+        name: 'Rahmat Fajri',
+        tag: 'rfajri27',
+        imageUrl: '/images/thomas.jpg',
+      },
+    ];
+   }
+    
+   export { getData };
